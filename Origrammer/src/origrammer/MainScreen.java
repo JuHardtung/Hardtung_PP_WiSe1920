a872ee990db4e7f3dc13aa5a5d1f1d8f45afbf41
Julian Hardtung <julian.hardtung@smail.fh-koeln.de>
2020-04-05 18:53:01 +0200

    several small fixes

--- Origrammer/src/origrammer/MainScreen.java
+++ Origrammer/src/origrammer/MainScreen.java
@@ -35,7 +35,6 @@ import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.EtchedBorder;
